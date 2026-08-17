f0316d7361b47687212e9c47fdeb7e736d59d89f
opabinia <urootsugi@gmail.com>
2026-08-17 11:41:20 +0900

    getEmbeddedDocument ではなく get する combat.combatants が combatants の collections だった

--- scripts/module.js
+++ scripts/module.js
@@ -5,13 +5,8 @@ Hooks.once("init", () => {
 });
 
 Hooks.on("combatTurnChange", (combat, previous, current) => {
-    const previousCombatant = previous.combatantId
-        ? combat.getEmbeddedDocument("Combatant", previous.combatantId)
-        : null;
-
-    const currentCombatant = current.combatantId
-        ? combat.getEmbeddedDocument("Combatant", current.combatantId)
-        : null;
+    const previousCombatant = combat.combatants.get(previous.combatantId) ?? null;
+    const currentCombatant = combat.combatants.get(current.combatantId) ?? null;
 
     const previousName = previousCombatant?.name ?? "None";
     const currentName = currentCombatant?.name ?? "None";
