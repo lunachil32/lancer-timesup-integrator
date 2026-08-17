48bc6086dec4c770fe314434199bf6a5bbb9a847
opabinia <urootsugi@gmail.com>
2026-08-17 20:22:44 +0900

    tweak 不要な定数を削除

--- scripts/module.js
+++ scripts/module.js
@@ -10,9 +10,6 @@ Hooks.on("combatTurnChange", (combat, previous, current) => {
     const previousCombatant = combat.combatants.get(previous.combatantId) ?? null;
     const currentCombatant = combat.combatants.get(current.combatantId) ?? null;
 
-    const previousName = previousCombatant?.name ?? "None";
-    const currentName = currentCombatant?.name ?? "None";
-
     if (previousCombatant) {
         onActivationEnd(previousCombatant);
     }
